b89e1774dec9a19d8f5958082cea94138ae8f469
Aaron Lamoreaux <aaronlamoreaux@Aarons-MacBook-Pro.local>
2021-08-15 00:08:47 -0400

    updated config

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,42 +1,26 @@
-const path = require('path')
-
 module.exports = {
   title: "Aaron Lamoreaux",
   description: "Aaron Lamoreaux's Personal Webpage",
-  head: [["link", { rel: "icon", href: `/logo.png` }]],
+  head: [
+    ["link", { rel: "icon", href: `/logo.png` }],
+    ["link", { rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.css" }]
+  ],
   base: "/",
   dest: "./dist",
 
   themeConfig: {
     search: false,
-    nav: [
-    ],
-    sidebar: {
-      '/guide/': genSidebarConfig('Guide')
-    },
+    nav: [],
+    sidebar: {},
     lastUpdated: 'Last Updated'
   },
 
   markdown: {
     anchor: { permalink: false },
     extendMarkdown: md => {
-      md.use(require("markdown-it-katex"));
+      md.use(require('markdown-it-katex'))
+      md.use(require('markdown-it-figure'))
+      md.use(require('markdown-it-html5-embed'), { html5embed: { useImageSyntax: true, useLinkSyntax: true } })
     }
   }
-};
-
-function genSidebarConfig (title) {
-  return [
-    {
-      title,
-      collapsable: false,
-      children: [
-        '',
-        'getting-started',
-        'customize',
-        'advanced',
-      ]
-    }
-  ]
 }
-
